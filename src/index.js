be43e844a545aa3d75b6bb052780c0b1ea473753
Josh <joshuamtryon@gmail.com>
2022-11-26 14:34:02 -0600

    Installed dev framework (Webpack, Babel, and React)

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import ReactDom from 'react-dom';
+import React from 'react';
+
+const App = () => {
+	return <h1>QUOTE MACHINE</h1>;
+}
+
+ReactDom.render(<App />, document.getElementById('main'));
