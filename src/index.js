a6c03b587d3105989b2f67c9ef0ca78d0c1fe24b
Josh <joshuamtryon@gmail.com>
2022-11-26 15:15:56 -0600

    prettier test 2

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-import ReactDom from 'react-dom';
-import React from 'react';
+import ReactDom from "react-dom";
+import React from "react";
 
-const App = () => {return <h1>QUOTE MACHINE</h1>;
-};ReactDom.render(<App />, document.getElementById('main'));
+const App = () => {
+  return <h1>QUOTE MACHINE</h1>;
+};
+ReactDom.render(<App />, document.getElementById("main"));
